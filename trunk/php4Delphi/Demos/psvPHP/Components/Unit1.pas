367e1112737d38340e9fce5c1c2ad96141608da9
delphi32 <delphi32@32167de5-9524-0410-b9c1-d9393829e5e2>
2007-01-03 20:26:35 +0000

    6.2.2

---
+++ trunk/php4Delphi/Demos/psvPHP/Components/Unit1.pas
@@ -0,0 +1,130 @@
+{*******************************************************}
+{                     PHP4Delphi                        }
+{               PHP - Delphi interface                  }
+{ Author:                                               }
+{ Serhiy Perevoznyk                                     }
+{ serge_perevoznyk@hotmail.com                          }
+{ http://users.chello.be/ws36637                        }
+{*******************************************************}
+
+{ $Id: Unit1.pas,v 6.2 02/2006 delphi32 Exp $ }
+
+//This demo project shows how to access published property
+//of Delphi components from PHP script
+
+{$I PHP.INC}
+
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  StdCtrls, php4delphi, PHPCommon;
+
+type
+
+  TMyButton = class(TButton)
+  private
+    function GetFontSize: integer;
+    procedure SetFontSize(const Value: integer);
+  published
+    property FontSize : integer read GetFontSize write SetFontSize;
+  end;
+
+  TfrmTest = class(TForm)
+    memScript: TMemo;
+    btnExecute: TButton;
+    PHP: TpsvPHP;
+    Label1: TLabel;
+    procedure btnExecuteClick(Sender: TObject);
+    procedure btnCloseClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    btnClose : TMyButton;
+  end;
+
+var
+  frmTest: TfrmTest;
+
+implementation
+
+{$R *.DFM}
+
+procedure TfrmTest.btnExecuteClick(Sender: TObject);
+begin
+  PHP.RunCode(memScript.Lines.Text);
+end;
+
+procedure TfrmTest.btnCloseClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TfrmTest.FormCreate(Sender: TObject);
+begin
+  btnClose := TMyButton.Create(Self);
+  with btnClose do
+  begin
+    Name := 'btnClose';
+    Parent := Self;
+    Left := 384;
+    Top := 42;
+    Width := 75;
+    Height := 25;
+    Caption := '&Close';
+    TabOrder := 2;
+    OnClick := btnCloseClick;
+  end;
+
+   memScript.Lines.Clear;
+   with memScript.Lines do
+   begin
+  {$IFDEF PHP4}
+    Add('$btnClose = register_delphi_component("btnClose");');
+    Add('$frmTest =   register_delphi_component("frmTest");');
+    Add('$btnClose->Caption = "&Exit";');
+    Add('$frmTest->Caption = "PHP4Delphi demo";');
+    Add('$btnClose->Top =  150;');
+    Add('$btnClose->Font->Size = 10;');
+    Add('$btnClose->Cursor = -21;');
+    Add('$frmTest->Color = clGreen;');
+    Add('$st = delphi_input_box("InputBox", "Type your message", "Done");');
+    Add('delphi_show_message($st);');
+  {$ELSE}
+    Add('$btnClose = register_delphi_component("btnClose");');
+    Add('$frmTest =   register_delphi_component("frmTest");');
+    Add('$btnClose->Caption = "&Exit";');
+    Add('$frmTest->Caption = "PHP4Delphi demo";');
+    Add('$btnClose->Top =  150;');
+    Add('$btnClose->FontSize = 10;');
+    Add('$btnClose->Cursor = -21;');
+    Add('$frmTest->Color = clGreen;');
+    Add('$st = delphi_input_box("InputBox", "Type your message", "Done");');
+    Add('delphi_show_message($st);');
+  {$ENDIF}
+   end;
+end;
+
+procedure TfrmTest.FormDestroy(Sender: TObject);
+begin
+   btnClose.Free;
+end;
+
+{ TMyButton }
+
+function TMyButton.GetFontSize: integer;
+begin
+  Result := Font.Size;
+end;
+
+procedure TMyButton.SetFontSize(const Value: integer);
+begin
+  Font.Size := Value;
+end;
+
+end.
---
+++ trunk/php4Delphi/Demos/psvPHP/array demo/Unit1.dfm
@@ -0,0 +1,54 @@
+object Form1: TForm1
+  Left = 490
+  Top = 294
+  BorderStyle = bsDialog
+  Caption = 'Array demo'
+  ClientHeight = 160
+  ClientWidth = 256
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poScreenCenter
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 16
+    Top = 64
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object ListBox1: TListBox
+    Left = 108
+    Top = 4
+    Width = 121
+    Height = 97
+    ItemHeight = 13
+    TabOrder = 1
+  end
+  object psvPHP1: TpsvPHP
+    Constants = <>
+    Variables = <>
+    Left = 20
+    Top = 16
+  end
+  object PHPLibrary1: TPHPLibrary
+    Executor = psvPHP1
+    LibraryName = 'ArrayLib'
+    Functions = <
+      item
+        FunctionName = 'get_php_array'
+        Tag = 0
+        Parameters = <>
+        OnExecute = PHPLibrary1Functions0Execute
+      end>
+    Left = 52
+    Top = 16
+  end
+end
